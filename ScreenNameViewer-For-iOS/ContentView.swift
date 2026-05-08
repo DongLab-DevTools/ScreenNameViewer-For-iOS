6b9b5879936f063d6998e2fb2033fbab2ba1b269
김동현 <dongdong99@kakao.com>
2026-05-09 02:46:27 +0900

    Initial Commit

---
+++ ScreenNameViewer-For-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScreenNameViewer-For-iOS
+//
+//  Created by 김동현 on 5/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScreenNameViewer-For-iOS/ScreenNameViewer_For_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenNameViewer_For_iOSApp.swift
+//  ScreenNameViewer-For-iOS
+//
+//  Created by 김동현 on 5/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ScreenNameViewer_For_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
